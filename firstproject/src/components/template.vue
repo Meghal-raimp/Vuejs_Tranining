0f15669afe6f47afe885b9116bca7b4eae5b5131
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-04-02 14:30:00 +0530

    first project setup and add template file in the component folder

---
+++ firstproject/src/components/template.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1 id="heading"> hello world</h1>
+    <p>{{string}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      string: 'Hello World'
+    }
+  },
+  beforeCreate() {
+    alert('before crate life cycle hook')
+  },
+  created() {
+    alert(this.string);
+    alert('created life cycle hook')
+  },
+  beforeMount() {
+    alert('before mounted life cycle hook')
+  },
+  mounted() {
+    alert('mounted life cycle hook');
+    this.updateData();
+  },
+  beforeUpdate() {
+    alert('before update life cycle hook')
+  },
+  updated() {
+    alert('updated life cycle hook')
+  },
+  methods:{
+    updateData(){
+      return this.string = "updated data";
+    }
+  }
+  // data come here
+  // lifeCycle will come here
+  // methods
+}
+</script>
+<style>
+  #heading{
+    color: red;
+    margin-top: 10px;
+  }
+</style>
+
+kassist 5676788
