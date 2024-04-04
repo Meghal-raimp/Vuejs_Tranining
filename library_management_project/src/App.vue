269fee588e0081eec4ef6c0852e1d60c5769894e
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-04-04 11:17:32 +0530

    basic changes

--- library_management_project/src/App.vue
+++ library_management_project/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <HomeView />
 </template>
+
+<script>
+import HomeView from "@/views/HomeView.vue"
+export default {
+  components:{
+    HomeView
+  }
+}
+</script>
 
 <style>
 #app {
